2876438b45c330ef1394ce494590cc4319f4f236
Lewin Kelly <heli@odex.cf>
2024-11-28 13:27:53 +0000

    More pages and content, basic crossfade view transitions

--- WDD/main.go
+++ WDD/main.go
@@ -10,8 +10,6 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-var layout = template.Must(template.ParseFiles("layout.html"))
-
 type Data struct {
 	Path, Title, Template string
 }
@@ -21,28 +19,36 @@ var pages = []Data{
 	{"/contact", "Contact", "pages/contact.html"},
 	{"/visit", "Visit", "pages/visit/index.html"},
 	{"/visit/walks", "Walks", "pages/visit/walks.html"},
+	{"/visit/café", "Café", "pages/visit/café.html"},
+	{"/visit/pettingzoo", "Petting zoo", "pages/visit/pettingzoo.html"},
 	{"/gallery", "Gallery", "pages/gallery.html"},
 }
 
 type Renderer struct{}
 
-func (r Renderer) Render(w io.Writer, name string, data any, c echo.Context) error {
+var pageData = map[string]map[string]any{
+	"pages/gallery.html": {
+		"Images": []int{1, 2, 3, 4, 5},
+	},
+}
+
+var layout = template.Must(template.ParseFiles("layout.html"))
+
+func (r Renderer) Render(w io.Writer, name string, data any, c echo.Context) (err error) {
 	t := template.Must(template.ParseFiles(name))
 
 	b := &strings.Builder{}
-	if err := t.ExecuteTemplate(b, t.Name(), data); err != nil {
-		return err
-	}
-
-	if err := layout.ExecuteTemplate(w, "layout.html", echo.Map{
+	if err = t.ExecuteTemplate(b, t.Name(), pageData[name]); err != nil {
+		return
+	} else if err = layout.ExecuteTemplate(w, "layout.html", echo.Map{
 		"Data": data,
 		"Body": template.HTML(b.String()),
 	}); err != nil {
 		fmt.Println(err)
-		return err
+		return
 	}
 
-	return nil
+	return
 }
 
 func main() {
@@ -62,6 +68,6 @@ func main() {
 		})
 	}
 
-	fmt.Println("Listening on :8080")
-	panic(e.Start(":8080"))
+	fmt.Println("Listening on :8090")
+	panic(e.Start(":8090"))
 }
