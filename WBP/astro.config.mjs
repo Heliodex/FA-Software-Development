8398ced4868c652c3cd5f13082af75b109409d2d
Lewin Kelly <heli@odex.cf>
2025-03-27 12:46:09 +0000

    Basic database setup

--- WBP/astro.config.mjs
+++ WBP/astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
+import db from "@astrojs/db"
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
+	integrations: [db()],
 })
---
+++ WBP/db/config.ts
@@ -0,0 +1,22 @@
+import { column, defineDb, defineTable } from "astro:db"
+
+const bookings = defineTable({
+	columns: {
+		area: column.text(),
+		date: column.date(),
+		startDate: column.date(),
+		endDate: column.date(),
+
+		bookerName: column.text(),
+		bookerEmail: column.text(),
+		bookerPhone: column.text(),
+		bookerPaymentInfo: column.text(),
+		paidDate: column.date({ optional: true }),
+	},
+})
+
+export default defineDb({
+	tables: {
+		bookings,
+	},
+})
